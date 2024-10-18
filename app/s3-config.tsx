391be3f552ef88f37ff510c87b4e822356ee4868
Daire <daire@protonmail.com>
2024-10-18 14:32:23 +0100

    Updated S3 config

--- app/s3-config.tsx
+++ app/s3-config.tsx
@@ -2,6 +2,7 @@ import {
   S3Client,
   PutObjectCommand,
   S3ServiceException,
+  paginateListObjectsV2,
 } from "@aws-sdk/client-s3";
 
 interface UploadFileParams {
@@ -10,19 +11,64 @@ interface UploadFileParams {
   file: File;
 }
 
+const client = new S3Client({
+  credentials: {
+    accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID || "",
+    secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY || "",
+  },
+  region: "eu-west-1",
+});
+
+/**
+ * Log all of the object keys in a bucket.
+ * @param {{ bucketName: string, pageSize: string }}
+ */
+export const listFiles = async ({ bucketName, pageSize }: { bucketName: string; pageSize: string; }) => {
+  /** @type {string[][]} */
+  const objects: string[][] = [];
+  try {
+    const paginator = paginateListObjectsV2(
+      { client, /* Max items per page */ pageSize: Number.parseInt(pageSize) },
+      { Bucket: bucketName },
+    );
+
+    for await (const page of paginator) {
+      if (page.Contents) {
+        objects.push(page.Contents.map((o) => o.Key).filter((key): key is string => key !== undefined));
+      }
+    }
+    console.log('objects', objects);
+    objects.forEach((objectList, pageNum) => {
+      console.log('objectList', objectList);
+      console.log(
+        `Page ${pageNum + 1}\n------\n${objectList.map((o) => `• ${o}`).join("\n")}\n`,
+      );
+    });
+    return objects;
+  } catch (caught) {
+    if (
+      caught instanceof S3ServiceException &&
+      caught.name === "NoSuchBucket"
+    ) {
+      console.error(
+        `Error from S3 while listing objects for "${bucketName}". The bucket doesn't exist.`,
+      );
+    } else if (caught instanceof S3ServiceException) {
+      console.error(
+        `Error from S3 while listing objects for "${bucketName}".  ${caught.name}: ${caught.message}`,
+      );
+    } else {
+      throw caught;
+    }
+  }
+}
+
 export const uploadFile = async ({
   bucketName,
   key,
   file,
 }: UploadFileParams) => {
   console.log("env:", process.env);
-  const client = new S3Client({
-    credentials: {
-      accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID || "",
-      secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY || "",
-    },
-    region: "eu-west-1",
-  });
   console.log("filePath:", file);
   const command = new PutObjectCommand({
     ACL: "public-read",
