605a3f658c980f04c16ce719dd99d5f6546a99af
Daire <daire@protonmail.com>
2024-12-30 20:38:49 +0000

    Updated page title

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import S3Uploader from "./uploader";
 import FileList from "./fileList";
+import Title from "antd/es/typography/Title";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>S3 Uploader</h1>
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start_">
+        <Title>S3 Uploader</Title>
 
         <S3Uploader />
         <FileList />
